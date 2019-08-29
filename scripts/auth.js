75f3c07d48a6fb7d793570c473f4e1d9913db497
Abayomi igwubor <igwubor@outlook.com>
2019-08-29 08:45:10 +0100

    addedAuthStateChange

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,3 +1,13 @@
+// listen for auth status changes
+auth.onAuthStateChanged((user) => {
+	if (user) {
+		console.log('user just logged in: ', user);
+	}
+	else {
+		console.log('user logged out');
+	}
+});
+
 // signup
 const signupForm = document.querySelector('#signup-form');
 signupForm.addEventListener('submit', (e) => {
@@ -9,7 +19,7 @@ signupForm.addEventListener('submit', (e) => {
 	console.log(email, password);
 	// sign up the user
 	auth.createUserWithEmailAndPassword(email, password).then((cred) => {
-		console.log(cred.user);
+		// console.log(cred.user);
 		// close the signup modal & reset form
 		const modal = document.querySelector('#modal-signup');
 		M.Modal.getInstance(modal).close();
@@ -21,7 +31,22 @@ signupForm.addEventListener('submit', (e) => {
 const logout = document.querySelector('#logout');
 logout.addEventListener('click', (e) => {
 	e.preventDefault();
-	auth.signOut().then(() => {
-		console.log('user signed out');
+	auth.signOut();
+});
+
+// login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	//    get user info
+
+	const email = loginForm['login-email'].value;
+	const password = loginForm['login-password'].value;
+	auth.signInWithEmailAndPassword(email, password).then((cred) => {
+		// console.log(cred.user);
+		const modal = document.querySelector('#modal-login');
+		M.Modal.getInstance(modal).close();
+		loginForm.reset();
 	});
 });
